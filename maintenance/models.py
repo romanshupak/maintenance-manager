753d66c2fda9cea08104d4a3f699aa285292a2fc
Roman Shupak <romanshupak808@gmail.com>
2024-05-08 16:22:52 +0300

    create and migrate models, create models.drawio

--- maintenance/models.py
+++ maintenance/models.py
@@ -1,3 +1,55 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers"
+    )
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name} {self.position})"
+
+    def get_absolute_url(self):
+        return reverse("maintenance:worker-detail", kwargs={"pk": self.pk})
+
+
+class Maintenance(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+        related_name="maintenances"
+    )
+    person_in_charge = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="maintenances"
+    )
+
+    class Meta:
+        ordering = ("name", )
