68bfdca4a6c6ddcd2a3ab2f62bb9313602f6c6e5
Roman Shupak <romanshupak808@gmail.com>
2024-05-14 14:17:13 +0300

    add additional field to Position model, fill in duties field in admin panel

--- maintenance/models.py
+++ maintenance/models.py
@@ -18,7 +18,8 @@ class Department(models.Model):
 
 
 class Position(models.Model):
-    name = models.CharField(max_length=255, unique=True)   #TODO add field duties, fill in admin panel, in detail list create duties for each rank
+    name = models.CharField(max_length=255, unique=True)
+    duties = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
--- maintenance_manager/settings.py
+++ maintenance_manager/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "maintenance",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -124,3 +126,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "maintenance.Worker"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
