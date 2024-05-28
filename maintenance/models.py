51ddfedcb8649afa4dee5511b2473ff76d579402
Roman Shupak <romanshupak808@gmail.com>
2024-05-28 14:03:23 +0300

    remove field duties from Position model and from models.drawio, create requirements.txt

--- maintenance/models.py
+++ maintenance/models.py
@@ -19,7 +19,6 @@ class Department(models.Model):
 
 class Position(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    duties = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
